572b34d19eb0c00f10882798ff2f1c6e34e042b5
alex <alex@nabu.be>
2015-07-03 08:01:44 +0200

    Added singletons

--- src/main/java/be/nabu/libs/types/properties/AttributeQualifiedDefaultProperty.java
+++ src/main/java/be/nabu/libs/types/properties/AttributeQualifiedDefaultProperty.java
@@ -3,6 +3,13 @@ package be.nabu.libs.types.properties;
 import be.nabu.libs.property.api.PropertyWithDefault;
 
 public class AttributeQualifiedDefaultProperty extends QualifiedProperty implements PropertyWithDefault<Boolean> {
+	
+	private static AttributeQualifiedDefaultProperty instance = new AttributeQualifiedDefaultProperty();
+	
+	public static AttributeQualifiedDefaultProperty getInstance() {
+		return instance;
+	}
+	
 	@Override
 	public String getName() {
 		return "attributeQualifiedDefault";
--- src/main/java/be/nabu/libs/types/properties/ElementQualifiedDefaultProperty.java
+++ src/main/java/be/nabu/libs/types/properties/ElementQualifiedDefaultProperty.java
@@ -3,6 +3,13 @@ package be.nabu.libs.types.properties;
 import be.nabu.libs.property.api.PropertyWithDefault;
 
 public class ElementQualifiedDefaultProperty extends QualifiedProperty implements PropertyWithDefault<Boolean> {
+	
+	private static ElementQualifiedDefaultProperty instance = new ElementQualifiedDefaultProperty();
+	
+	public static ElementQualifiedDefaultProperty getInstance() {
+		return instance;
+	}
+	
 	@Override
 	public String getName() {
 		return "elementQualifiedDefault";

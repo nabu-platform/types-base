211b119fc7b798d684cfc6dfa28dfbbf9af5fdd7
alex <alex@nabu.be>
2016-06-28 11:13:01 +0200

    Added same fix to float as was added to double for serialization

--- src/main/java/be/nabu/libs/types/simple/Float.java
+++ src/main/java/be/nabu/libs/types/simple/Float.java
@@ -16,7 +16,25 @@ public class Float extends BaseComparableSimpleType<java.lang.Float> {
 
 	@Override
 	public java.lang.String marshal(java.lang.Float object, Value<?>...values) {
-		return object == null ? null : object.toString().replaceAll("[.0]+$", "");
+		if (object == null) {
+			return null;
+		}
+		else {
+			java.lang.String string = object.toString();
+			int index = string.indexOf('.');
+			if (index >= 0) {
+				// strip trailing 0
+				string = string.replaceAll("[0]+$", "");
+				if (string.endsWith(".")) {
+					// strip the trailing "."
+					string = string.substring(0, string.length() - 1);
+				}
+			}
+			if (string.isEmpty()) {
+				string = "0";
+			}
+			return string;
+		}
 	}
 
 	@Override

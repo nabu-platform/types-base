4cc37c41584c372eacf3a49014c9eb668870ccbd
Alexander Verbruggen <alex@nabu.be>
2017-08-17 17:01:01 +0200

    Standardized string > boolean conversion

--- src/main/java/be/nabu/libs/types/simple/Boolean.java
+++ src/main/java/be/nabu/libs/types/simple/Boolean.java
@@ -1,7 +1,6 @@
 package be.nabu.libs.types.simple;
 
 import be.nabu.libs.property.api.Value;
-import be.nabu.libs.types.api.MarshalException;
 import be.nabu.libs.types.api.Unmarshallable;
 import be.nabu.libs.types.base.BaseMarshallableSimpleType;
 
@@ -26,10 +25,22 @@ public class Boolean extends BaseMarshallableSimpleType<java.lang.Boolean> imple
 		// the default boolean parsing in java is (surprisingly) too lenient
 		// if you try "new java.lang.Boolean('this is definitely not a boolean')" it will be the value false
 		// we want an exception indicating that what you sent is not a valid boolean in any way
-		if (content != null && !content.equalsIgnoreCase("true") && !content.equalsIgnoreCase("false")) {
-			throw new MarshalException("The string is not a correct boolean value: " + content);
+		// update: due to priorities of converters and multiple string > boolean paths (in some setups) with equal priority
+		// it becomes too hard to only make this path more strict as it may lead to seemingly "random" failures depending on converter order
+//		if (content != null && !content.equalsIgnoreCase("true") && !content.equalsIgnoreCase("false")) {
+//			throw new MarshalException("The string is not a correct boolean value: " + content);
+//		}
+		
+		// keep in sync with be.nabu.libs.converter.base.providers.StringToBoolean for consistent results cross environment
+		if (content == null) {
+			return null;
 		}
-		return content == null ? null : new java.lang.Boolean(content);
+		else if (content.matches("[0-9]+")) {
+			return java.lang.Integer.parseInt(content) >= 1;
+		}
+		else {
+			return java.lang.Boolean.parseBoolean(content);
+		}
 	}
 
 	@Override

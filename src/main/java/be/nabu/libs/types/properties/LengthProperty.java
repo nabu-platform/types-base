f5c510edda973f259d732e2368849fcfd4e9a665
Alexander Verbruggen <alex@nabu.be>
2021-03-11 14:27:34 +0100

    updated

--- src/main/java/be/nabu/libs/types/properties/LengthProperty.java
+++ src/main/java/be/nabu/libs/types/properties/LengthProperty.java
@@ -1,7 +1,15 @@
 package be.nabu.libs.types.properties;
 
 public class LengthProperty extends SimpleProperty<Integer> {
-
+	private static LengthProperty instance;
+	
+	public static LengthProperty getInstance() {
+		if (instance == null) {
+			instance = new LengthProperty();
+		}
+		return instance;
+	}
+	
 	public LengthProperty() {
 		super(Integer.class);
 	}
